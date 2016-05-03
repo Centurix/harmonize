9b8f52d6af7c20a6f68af81ecc37254cb22438fb
Chris Read <centurix@gmail.com>
2016-05-03 23:17:06 +1000

    Check and download harmontown episodes automatically

--- harmontown.py
+++ harmontown.py
@@ -1,11 +1,10 @@
-#!python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 import sys
 import requests
 import os
-import pprint
-from time import gmtime, strftime, sleep
+import datetime
 
 __description__ = 'Harmontown grabber.'
 __author__ = 'Chris Read'
@@ -17,42 +16,44 @@ EXIT_OTHER_ERROR = 1
 EXIT_PARAM_ERROR = 2
 
 """
-http://download.harmontown.com/video/harmontown-2016-05-01-final.mp4
-http://download.harmontown.com/video/harmontown-2016-04-24-final.mp4
-http://download.harmontown.com/video/harmontown-2016-04-17-final.mp4
-http://download.harmontown.com/video/harmontown-2016-04-10-final.mp4
-http://download.harmontown.com/video/harmontown-2016-04-03-final.mp4
-http://download.harmontown.com/video/harmontown-2016-03-27-final.mp4
-http://download.harmontown.com/video/harmontown-2016-03-20-final.mp4
-http://download.harmontown.com/video/harmontown-2016-03-13-final.mp4
+http://download.harmontown.com/video/harmontown-2015-01-25-final.mp4
 """
 
 HARMONTOWN_URL = 'http://download.harmontown.com/video'
 HARMONTOWN_DIRECTORY = 'temp'
+HARMONTOWN_START = datetime.date(2015, 1, 25)
+
+delta = datetime.timedelta(days=1)
 
 
 def main():
-    # file_name = 'harmontown-%s-final.mp4' % strftime('%Y-%m-%d')
-    file_name = 'harmontown-2016-05-01-final.mp4'
-    print('%s/%s' % (HARMONTOWN_URL, file_name))
-    bytes = 0
-    files = os.listdir(HARMONTOWN_DIRECTORY)
-    with open(os.path.join(HARMONTOWN_DIRECTORY, file_name), 'wb') as handle:
-        result = requests.get('%s/%s' % (HARMONTOWN_URL, file_name), stream=True)
-        if not result.ok:
-            print('No episode found')
-            return EXIT_OK
+    start_date = HARMONTOWN_START
+    try:
+        with open('last_date', 'r') as handle:
+            start_date = datetime.datetime.strptime(handle.readline(), '%Y-%m-%d').date()
+    except IOError as ie:
+        print('No last episode found, starting from the beginning')
+    end_date = datetime.datetime.now().date()
+    while start_date <= end_date:
+        file_name = 'harmontown-%s-final.mp4' % start_date
+        request = requests.get('%s/%s' % (HARMONTOWN_URL, file_name), stream=True)
+        if request.ok:
+            print(start_date)
+            size = int(request.headers['content-length'])
+            bytes_received = 0
+            with open(os.path.join(HARMONTOWN_DIRECTORY, file_name), 'wb') as handle:
+                for block in request.iter_content(1024):
+                    print('Written %d Kb of %d Kb (%.2f%% complete)' % (
+                        bytes_received / 1024,
+                        size / 1024,
+                        float(bytes_received) / float(size) * 100
+                    ), end="\r")
+                    bytes_received += 1024
+                    handle.write(block)
 
-        size = int(result.headers['content-length'])
-
-        for block in result.iter_content(1024):
-            print('Written %d Kb of %d Kb (%.2f%% complete)' % (bytes / 1024, size / 1024, float(bytes) / float(size) * 100), end="\r")
-            # sys.stdout.write()
-            # sys.stdout.flush()
-
-            sleep(.01)
-            bytes += 1024
-            handle.write(block)
+            with open('last_date', 'w') as handle:
+                handle.write(start_date.strftime('%Y-%m-%d'))
+        start_date += delta
 
     return EXIT_OK
 
